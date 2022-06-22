635a655f5c051480dc86692c8440f6363635543c
Rawaz Ali <rawazali899@gmail.com>
2022-06-22 17:14:13 +0300

    adding sidebar

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("open").style.display = "none";
+
+
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("open").style.display = "block";
+
+}
