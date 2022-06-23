907fe56d78ac06472073b4d8fde5abf7f67ef305
Rawaz Ali <rawazali899@gmail.com>
2022-06-23 11:14:48 +0300

    changing some design, adding navbar for a media query

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-
-function openNav() {
+function openSideNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("open").style.marginLeft = "250px";
   document.getElementById("open").style.display = "none";
+  document.getElementById("content").style.marginLeft = "260px";
+  document.getElementById("content").style.marginTop = "20px";
+
 
 
 }
 
-function closeNav() {
+function closeSideNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("open").style.marginLeft = "0px";
   document.getElementById("open").style.display = "block";
+  document.getElementById("content").style.marginLeft = "10px";
+  document.getElementById("content").style.marginTop = "0";
+
 
 }
