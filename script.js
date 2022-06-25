86d57f823f93c37a5b01590abfcb9f4669d15bd1
Rawaz Ali <rawazali899@gmail.com>
2022-06-25 12:19:53 +0300

    adding about section, remove media query , fixing sidebar

--- script.js
+++ script.js
@@ -1,20 +1,9 @@
 function openSideNav() {
   document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("open").style.marginLeft = "250px";
-  document.getElementById("open").style.display = "none";
-  document.getElementById("content").style.marginLeft = "260px";
-  document.getElementById("content").style.marginTop = "20px";
-
-
 
 }
 
 function closeSideNav() {
   document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("open").style.marginLeft = "0px";
-  document.getElementById("open").style.display = "block";
-  document.getElementById("content").style.marginLeft = "10px";
-  document.getElementById("content").style.marginTop = "0";
-
 
 }
