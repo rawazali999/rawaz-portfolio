a1d3094b231798b127130a82875de5b4abe618a4
Rawaz Ali <rawazali899@gmail.com>
2022-06-26 03:21:04 +0300

    adding project section

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function openSideNav() {
   document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("header").style.marginLeft = "250px";
+  document.getElementById("open").style.display = "none";
 
 }
 
 function closeSideNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("header").style.marginLeft = "0px";
+  document.getElementById("open").style.display = "block";
+
 
 }
 
